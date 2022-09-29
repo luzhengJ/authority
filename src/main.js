4d36a3cfcf1f3897039f32d3f921aea48eb9a79f
鲁正佼 <3038413299@qq.com>
2022-09-29 10:55:42 +0800

    feat(custom): 二次封装axios以及 不同传参方式都使用data

--- src/main.js
+++ src/main.js
@@ -5,11 +5,9 @@ import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-import axios from "axios"
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
-Vue.prototype.$axios =axios
 
 new Vue({
   router,
---
+++ src/utils/request.js
@@ -0,0 +1,37 @@
+
+// 引入axios
+import axios from "axios"
+
+const Service =axios.create({
+    baseURL:"process.env.VUE_APP_BASE_API",
+    timeout:5000
+})
+
+// 请求拦截
+Service.interceptors.request.use(function (config) {
+    return config;
+  }, function (error) {
+    return Promise.reject(error);
+  });
+
+
+//   响应拦截
+Service.interceptors.response.use(function (response) {
+    return response;
+  }, function (error) {
+    return Promise.reject(error);
+  });
+
+
+//   不同的传参方式都使用data
+const request = (options) =>{
+    options.method = options.method || 'get'
+    if(options.method.toLowerCase() === 'get'){
+        options.params=options.data || options.params
+        delete options.data
+    }
+    return Service(options)
+}
+
+// 导出
+export default Service
