345b226bd010c2b302c88997692270acda840d60
鲁正佼 <3038413299@qq.com>
2022-09-29 10:26:02 +0800

    feat(custom): 配置跨域

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,28 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    // 端口号
+    port:8080,
+    // 主机号
+    host:"localhost",
+    // 开启项目时是否自动打开浏览器
+    open:true,
+    // 是否开启https
+    https:false,
+    // 配置跨域
+    proxy:{
+      // 代理名称
+     [process.env.VUE_APP_BASE_API]:{
+        // 跨域地址
+        target:process.env.VUE_APP_SERVICE_URL,
+        // 开启跨域
+        changeOrigin:true,
+        // 路径重写
+        pathRewrite:{
+          ["^"+process.env.VUE_APP_BASE_API]:""
+        }
+      }
+    }
+  }
 })
